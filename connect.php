56b16987324fbdae524e24e3c85e4bb45769d622
msaleh7 <49427869+msaleh7@users.noreply.github.com>
2019-04-12 13:57:59 -0700

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,9 +1,24 @@
+
+
+
+
 <?php
-$conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "C:/ssl/wwwdigicertcom.crt", NULL, NULL) ;
-mysqli_real_connect($conn, 'mysqldb7.mysql.database.azure.com', 'msaleh7', 'Msm@9810959', 'blogdb', 3306, MYSQLI_CLIENT_SSL);
-if (mysqli_connect_errno($conn)) {
-    die('Failed to connect: '.mysqli_connect_error());
+
+
+$conn = new mysqli();
+$conn->init();
+
+// below contains certificate path required in azure
+$conn->ssl_set(NULL,NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); // set SSL options
+
+$conn->real_connect('mysqldb7.mysql.database.azure.com', 'msaleh7', 'Msm@9810959', 'mysqldb7', 3306, MYSQLI_CLIENT_SSL); // actually establish connection via port 3306 using ssl
+
+if ($conn->connect_error) {
+    die ("Could not connect. ".$conn->connect_error);
 }
 
+
 ?>
+
+
+
